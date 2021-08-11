0dcf2922ea2ddfb5eda8467fde991019ae7c6923
harsh9975 <gautam33776@gmail.com>
2021-08-11 12:02:27 +0530

    chat section done

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,9 +1,92 @@
 import React from 'react'
+import "./Chat.css"
+import { Avatar,IconButton } from "@material-ui/core";
+import {SearchOutlined, MoreVert, AttachFile} from '@material-ui/icons'
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>Chatbar</h1>
+            <div className="chat-header">
+                <Avatar/>
+                <div className="chat-header-info">
+                    <h3>Name</h3>
+                    <p>last seen at</p>
+                </div>
+
+                <div className="chat-header-right">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat-body">
+                <p className="chat-message" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message chat-rec" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message chat-rec" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message chat-rec" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message chat-rec" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat-message chat-rec" >
+                   <span className="chat-name">Harish</span>
+                    This is a message
+                    <span className="chat-timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
         </div>
     )
 }
