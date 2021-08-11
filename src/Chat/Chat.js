76a36dad745ed54446e27aaaf1322f312b74cd5b
harsh9975 <gautam33776@gmail.com>
2021-08-11 09:38:34 +0530

    initial setup

---
+++ src/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h1>Chatbar</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBskMO3KgOyiIKOTzfcsPmyVSd4TKCtGog",
+    authDomain: "mern-whatsapp-1afca.firebaseapp.com",
+    projectId: "mern-whatsapp-1afca",
+    storageBucket: "mern-whatsapp-1afca.appspot.com",
+    messagingSenderId: "346817775593",
+    appId: "1:346817775593:web:c777df812fe94c7270ed78",
+    measurementId: "G-8093PYHJ7F"
+  };
